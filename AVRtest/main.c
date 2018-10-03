5530ab95475a3b11b087b7dc1dd534034fddd23c
RAZMI <RAZMI@RAZMI-PC>
2018-10-03 08:18:29 +0530

    Switch added

--- AVRtest/main.c
+++ AVRtest/main.c
@@ -7,15 +7,20 @@
 
 #include<avr\io.h>
 #include<util\delay.h>
+#define SW PIND & 0X02
 int main()
 {
+	DDRD &= ~(1<<2);
 	DDRA = 0XFF;
 	PORTA = 0XFF;
 	while(1)
 	{
-
+		if (!SW)
+		{
 		PORTA = ~ PORTA;
 		_delay_ms(1000);
+		}
+
 	}
 
 	return 0;
